92ed476485300612c5672a1f99f6a084bb8fb2b6
Terri Burns <terri.burns@nyu.edu>
2016-01-29 16:20:22 -0500

    Prelim link to press page

--- app/scripts/Main.js
+++ app/scripts/Main.js
@@ -1,9 +1,13 @@
 var react = require('react');
+var Press = require('press');
 
 var Main = React.createClass({
   render: function() {
     return {
-      <div>This is the Main page</div>
+      <div>
+        This is the Main page
+        Click <a href="#click">here</a> to go to the press page.
+      </div>
     };
   }
 });
