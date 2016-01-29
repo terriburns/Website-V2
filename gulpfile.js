681c171a817e8cee7d4a7a727055e4b6afc865c6
Terri Burns <terri.burns@nyu.edu>
2016-01-29 00:46:28 -0500

    Creating gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,14 @@
-require("coffee-script/register");
-require("./gulpfile.coffee");
-
 var gulp = require('gulp');
 var react = require('gulp-react');
 var browserify = require('gulp-browserify');
-
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var htmlreplace = require('gulp-html-replace');
 
 var path = {
   HTML: 'index.html',
-  ALL: ['app/scripts/*.js', 'index.html'],
-  JS: ['app/scripts/*.js'],
+  ALL: ['scripts/*.js', 'srcipts/**/*.js', 'index.html'],
+  JS: ['srcipts/*.js', 'srcipts/**/*.js'],
   MINIFIED_OUT: 'build.min.js',
   DEST_SRC: 'dist/src',
   DEST_BUILD: 'dist/build',
@@ -25,16 +21,23 @@ gulp.task('transform', function(){
   .pipe(gulp.dest(path.DEST_SRC));
 });
 
-/*gulp.task('default', function () {
-  return gulp.src('template.jsx')
-  .pipe(react())
-  .pipe(gulp.dest('dist'));
+gulp.task('copy', function(){
+  gulp.src(path.HTML)
+  .pipe(gulp.dest(path.DEST));
 });
 
-gulp.task('scripts', function() {
-  gulp.src('src/js/App.js')
-  .pipe(browserify({
-    insertGlobals : true,
-  }))
-  .pipe(gulp.dest('/scripts/builds'))
-});*/
+gulp.task('watch', function(){
+  gulp.watch(path.ALL, ['transform', 'copy']);
+});
+
+gulp.task('build', function(){
+  gulp.src(path.JS)
+  .pipe(react())
+  .pipe(concat(path.MINIFIED_OUT))
+  .pipe(uglify(path.MINIFIED_OUT))
+  .pipe(gulp.dest(path.DEST_BUILD));
+});
+
+gulp.task('default', ['watch']);
+
+gulp.task('production', ['replaceHTML', 'build']);
